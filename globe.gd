1d3a81eec228029db8ee17cd9f893fa9436cb75b
NahiyanKhandaker <nahiyank09@gmail.com>
2026-07-05 18:31:56 +0100

    added more features to things like settings

--- globe.gd
+++ globe.gd
@@ -2,6 +2,9 @@ extends Node
 
 var minigames_done = 0 #track how many minigames done
 var lives = 5 #track how many lives left, also effecting garlic appearing
+var mario = true #tracks if user wants to use mario or luigi
+var rollingBackground = true
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
