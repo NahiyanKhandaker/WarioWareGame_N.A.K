d861e2291a99500bfe5a8c8a5beac2bf68d9b910
LadiesMan225 <nahiyank09@gmail.com>
2026-07-06 20:42:38 +0100

    added some extra features

--- globe.gd
+++ globe.gd
@@ -3,7 +3,8 @@ extends Node
 var minigames_done = 0 #track how many minigames done
 var lives = 5 #track how many lives left, also effecting garlic appearing
 var mario = true #tracks if user wants to use mario or luigi
-var rollingBackground = true
+var rollingBackground = true #stops whether background rolls or not
+var flappy = true #tracks if user wants to use flappy bird or red
 
 
 # Called when the node enters the scene tree for the first time.
