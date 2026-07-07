df95650bce0fa6c4fcaffb764df743d3183ffb12
NahiyanKhandaker <nahiyank09@gmail.com>
2026-07-07 23:02:10 +0100

    I am basically finished just need to fix few bugs

--- globe.gd
+++ globe.gd
@@ -4,6 +4,8 @@ var minigames_done = 0 #track how many minigames done
 var lives = 5 #track how many lives left, also effecting garlic appearing
 var mario = true #tracks if user wants to use mario or luigi
 var rollingBackground = true
+var flappy = true #tracks if user wants to use flappy bird or red
+var botDifficulty = "easy"
 
 
 # Called when the node enters the scene tree for the first time.
