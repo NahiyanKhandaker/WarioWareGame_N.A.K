4b81ebdd10fded2b84ebc60caa12a80d854230d4
NahiyanKhandaker <nahiyank09@gmail.com>
2026-07-08 15:07:11 +0100

    now it should finnally be done

--- globe.gd
+++ globe.gd
@@ -6,14 +6,3 @@ var mario = true #tracks if user wants to use mario or luigi
 var rollingBackground = true #stops whether background rolls or not
 var flappy = true #tracks if user wants to use flappy bird or red
 var botDifficulty = "easy"
-
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
