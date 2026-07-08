513449a72df83a49705dedbf171cff3c6c3ac3f5
NahiyanKhandaker <nahiyank09@gmail.com>
2026-07-08 13:39:02 +0100

    finished game lets go dudes

--- globe.gd
+++ globe.gd
@@ -8,6 +8,7 @@ var flappy = true #tracks if user wants to use flappy bird or red
 var botDifficulty = "easy"
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
